637a0c2e6c366513b12fdc98eac2e5af3d6400b2
chenanqi1024 <chenanqi1024@gmail.com>
2026-07-19 20:53:57 +0800

    Initial Commit

---
+++ 图文APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  图文APP
+//
+//  Created by chenanqi on 2026/7/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 图文APP/__APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  __APPApp.swift
+//  图文APP
+//
+//  Created by chenanqi on 2026/7/19.
+//
+
+import SwiftUI
+
+@main
+struct __APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
